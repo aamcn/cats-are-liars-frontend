f666604f824fbf871198b844c4438c6a37b7ab0c
malcomcrab <malcomcrabesq@outlook.com>
2025-05-09 13:42:26 +0100

    Style: Add css modules and styling for MyCatsDisplayCard component and its children

--- src/components/myCatsPage/CatDisplayCardTemplate.jsx
+++ src/components/myCatsPage/CatDisplayCardTemplate.jsx
@@ -1,21 +1,42 @@
 import { Link } from "react-router";
-
+import styles from "./css/catDisplayCardTemplate.module.css"
 function CatDisplayCardTemplate({ cat }) {
   const catName = cat.name;
   return (
-    <tr>
-      <td>
-        <Link to={`/cat-view/${catName}`}>{catName}</Link>
-      </td>
-      <td>
-        {cat.meals.map((meal) => {
-          return <p>{meal}</p>;
-        })}
-      </td>
-      <td>
-        <p>{cat.medication}</p>
-      </td>
-    </tr>
+
+    <div className={styles.displayCardContainer}>
+
+      <div className={styles.cardHeader}>
+        <img className={styles.catCardPhoto} src={null} alt="Cat Photo" />
+        <h2 className={styles.catName}>{cat.name}</h2>
+      </div>
+
+      
+      
+      <div className={styles.catDetailsContainer}>
+        <h3 className={styles.catDetailsTitle}>Meals:</h3>
+        <ul className={styles.catDetailsList}>
+          {cat.meals.map(item => {
+            return <li>{item}</li>
+          })}
+        </ul>
+        
+        <h3 className={styles.catDetailsTitle}>Meds:</h3>
+        <ul className={styles.catDetailsList}>
+          {cat.medication.map(item => {
+            return <li>{item}</li>
+          })}
+        </ul> 
+        {/* <h3>Feeders:</h3>
+                  <hr/>
+                  <ul>
+                    {feeders.map(item => {
+                      return <li>{item}</li>
+                    })}
+                  </ul> */}
+        </div>
+    </div>
+   
   );
 }
 
--- src/components/myCatsPage/MyCatsDisplayCard.jsx
+++ src/components/myCatsPage/MyCatsDisplayCard.jsx
@@ -1,22 +1,12 @@
 import CatDisplayCardTemplate from "./CatDisplayCardTemplate";
-import styles from "./css/MyCatsDisplayCard.module.css";
+import styles from "./css/myCatsDisplayCard.module.css";
 function MyCatsDisplayCard({ cats }) {
   return (
-    <div className={styles.userCatsTable}>
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Meals</th>
-            <th>Meds</th>
-          </tr>
-        </thead>
-        <tbody>
+    <div className={styles.cardsContainer}>
+        
           {cats.map((cat) => {
             return <CatDisplayCardTemplate cat={cat} />;
           })}
-        </tbody>
-      </table>
     </div>
   );
 }
