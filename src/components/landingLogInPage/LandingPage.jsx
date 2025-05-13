830ee46468a66583adfcc8b8f67f8c2e72363afc
malcomcrab <malcomcrabesq@outlook.com>
2025-05-13 16:03:52 +0100

    Style: Add styling to LandingPage and LogInForm components to match app theme

--- src/components/landingLogInPage/LandingPage.jsx
+++ src/components/landingLogInPage/LandingPage.jsx
@@ -5,7 +5,11 @@ function LandingPage() {
   return (
     <div className={styles.landingPageContainer}>
       <h1 className={styles.pageTitle}>Cats Are Liars</h1>
-      <LogInForm />
+      <div className={styles.formContainer}>
+        <LogInForm />
+      </div>
+      <div className={styles.pageFooter}>
+      </div>
     </div>
   );
 }
