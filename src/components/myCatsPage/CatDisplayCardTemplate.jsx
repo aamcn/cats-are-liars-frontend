adaeda48faa5472c1541953a2be787846e0ace15
malcomcrab <malcomcrabesq@outlook.com>
2025-05-14 17:55:30 +0100

    Feat: Add visibility toggle for each CatDisplayCard on MyCatsPage

--- src/components/myCatsPage/CatDisplayCardTemplate.jsx
+++ src/components/myCatsPage/CatDisplayCardTemplate.jsx
@@ -1,32 +1,57 @@
 import { Link } from "react-router";
+import { useState } from "react";
+import DownChevron from "../../assets/svg/doubleDownChevron.svg?react"
+import MinimiseIcon from "../../assets/svg/minimiseIcon.svg?react"
 import styles from "./css/catDisplayCardTemplate.module.css";
+
+
+
 function CatDisplayCardTemplate({ cat }) {
-  const catName = cat.name;
+  const [isCardHidden, setIsCardHidden] = useState(false)
+
+  const handleToggleCard = () => {
+    if (isCardHidden == true) {
+      setIsCardHidden(false);
+      setIsCardHidden(false);
+    } else {
+      setIsCardHidden(true);
+    }
+  };
+
   return (
     <div className={styles.displayCardContainer}>
-      <div className={styles.cardHeader}>
-        <img className={styles.catCardPhoto} src={null} alt="Cat Photo" />
+
+
+      <div className={styles.tabMenuContainer}>
         <h2 className={styles.catName}>{cat.name}</h2>
+        <button onClick={handleToggleCard} className={styles.tabButton}>{cat ? <MinimiseIcon className={styles.minimiseIcon} /> : <DownChevron className={styles.downChevron} />}</button>
       </div>
 
-      <div className={styles.catDetailsContainer}>
-        <h3 className={styles.catDetailsTitle}>Meals:</h3>
-        <ul className={styles.catDetailsList}>
-          {cat.meals.map((item) => {
-            return <li>{item}</li>;
-          })}
-        </ul>
-        <h3 className={styles.catDetailsTitle}>Meds:</h3>
-        <ul className={styles.catDetailsList}>
-          {cat.medication.map((item) => {
-            return <li>{item}</li>;
-          })}
-        </ul>
-        <h3>Feeders:</h3>
-        <ul>
-          <li>Tony</li>
-        </ul>
-      </div>
+
+      {isCardHidden && <div>
+        <div className={styles.cardHeader}>
+          <img className={styles.catCardPhoto} src={null} alt="Cat Photo" />
+         
+        </div>
+        <div className={styles.catDetailsContainer}>
+          <h3 className={styles.catDetailsTitle}>Meals:</h3>
+          <ul className={styles.catDetailsList}>
+            {cat.meals.map((item) => {
+              return <li>{item}</li>;
+            })}
+          </ul>
+          <h3 className={styles.catDetailsTitle}>Meds:</h3>
+          <ul className={styles.catDetailsList}>
+            {cat.medication.map((item) => {
+              return <li>{item}</li>;
+            })}
+          </ul>
+          <h3>Feeders:</h3>
+          <ul>
+            <li>Tony</li>
+          </ul>
+        </div>
+      </div>}
     </div>
   );
 }
