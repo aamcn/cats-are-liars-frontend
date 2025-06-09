7f4c332f2fc994971f1c51d4c0807e89271bc9be
malcomcrab <malcomcrabesq@outlook.com>
2025-06-09 17:08:21 +0100

    Refactor: Add new footer and form toggle function Add new footer component and configure the function to toggle the 'AddCatForm' component

--- src/components/myCatsPageComponents/myCatsPage/MyCatsPage.jsx
+++ src/components/myCatsPageComponents/myCatsPage/MyCatsPage.jsx
@@ -1,12 +1,15 @@
 import { use, useEffect, useState } from "react";
 import MyCatsDisplayCard from "../catDisplayCard/MyCatsDisplayCard";
+import AddCatForm from "../../addCatForm/AddCatForm";
+import Footer from "../../footer/Footer";
 import axios from "axios";
 import "./myCatsPage.scss";
 import PawIcon from "../../../assets/svg/paw.svg?react";
 
 function MyCatsPage() {
   const [cats, setCats] = useState([]);
-  const [isHidden, setIsHidden] = useState(true);
+  const [formToDisplay, setFormToDisplay] = useState(null);
+
 
   const username = localStorage.getItem("username").replaceAll('"', "");
   const userId = localStorage.getItem("userId").replaceAll('"', "");
@@ -31,7 +34,13 @@ function MyCatsPage() {
     getCats();
   }, []);
 
-  
+  const toggleFormDisplay = (event) => {
+    if (formToDisplay != event.target.value) {
+      setFormToDisplay(event.target.value);
+    } else {
+      setFormToDisplay(null);
+    }
+  };
 
   return (
     <div className="myCatsPageContainer">
@@ -41,8 +50,10 @@ function MyCatsPage() {
           <PawIcon height="1em" width="1em" />
         </h1>
       </div>
-      
+      {formToDisplay == 'Add a Cat' && <AddCatForm  toggleFormDisplay={toggleFormDisplay}/>}
+
       <MyCatsDisplayCard cats={cats} />
+      <Footer formToggle={toggleFormDisplay}  formNames={['Add a Cat']}/>
     </div>
   );
 }
