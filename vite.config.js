8290976571df88a8f96c80c4ff7705807a159b0e
aaronmcnulty <malcomcrabesq@outlook.com>
2025-06-23 15:59:03 +0100

    Build: Install and configure Vitest and jsdom

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svgr(), react()],
+  plugins: [react()],
+  base: "/cats-are-liars-frontend",
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./tests/setup.js",
+  },
 });
