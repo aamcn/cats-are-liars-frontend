2184a734b9cd4a9c1c990692b084572652d91164
aaronmcnulty <malcomcrabesq@outlook.com>
2025-08-22 19:26:49 +0100

    Bug: Setup browserRouterrendering to fix error message

--- src/components/addCatForm/AddCatForm.test.jsx
+++ src/components/addCatForm/AddCatForm.test.jsx
@@ -1,16 +1,32 @@
+import AddCatForm from "./AddCatForm";
 import { describe, it, expect } from "vitest";
 // import userEvent from "@testing-library/user-event";
 import { render, screen } from "@testing-library/react";
-import AddCatForm from "./AddCatForm";
+import { BrowserRouter } from "react-router-dom";
+
+const renderWithRouter = (container) => {
+  return render(
+    <BrowserRouter>
+      {container}
+    </BrowserRouter>
+  );
+};
+
+const localStorage = {
+  setItem: (key, value) => {
+    window.localStorage.setItem(key, value);
+  },
+  getItem: (key) => {
+    return window.localStorage.getItem(key);
+  },
+};
 
 describe("Add Cat Form", () => {
 
     localStorage.setItem('storedToken', 'mockToken')
 
  it("Should render the 'Add Cat Form component' ", () => {
-    render(
-          <AddCatForm />
-    )
+    renderWithRouter(<AddCatForm />);
     const formContainer = screen.getByTestId("addCatForm");
 
     expect(formContainer).toBeInTheDocument()
