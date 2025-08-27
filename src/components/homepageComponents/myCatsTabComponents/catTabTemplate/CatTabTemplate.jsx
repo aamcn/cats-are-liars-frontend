fdf83efb8e8dc005c9d9a4a3a5ef690e8d38d904
aaronmcnulty <malcomcrabesq@outlook.com>
2025-08-27 22:49:36 +0100

    Test: Add test suite and tests for LandingPage component

--- src/components/homepageComponents/myCatsTabComponents/catTabTemplate/CatTabTemplate.jsx
+++ src/components/homepageComponents/myCatsTabComponents/catTabTemplate/CatTabTemplate.jsx
@@ -2,7 +2,7 @@ import "./catTabTemplate.scss";
 import { Link } from "react-router";
 function CatTabTemplate({ cat }) {
   return (
-    <div className="catTabCard">
+    <div className="catTabCard" data-testid="cat-card-container">
       <img className="catTabProfilePhoto" src={null} alt="Cat Photo"></img>
       <Link className="catTabNameLink" to={`/cat-view/${cat.name}`}>
         {cat.name}
---
+++ src/components/homepageComponents/myCatsTabComponents/catTabTemplate/catTabTemplate.test.jsx
@@ -0,0 +1,55 @@
+import CatTabTemplate from "./CatTabTemplate";
+import { describe, it, expect, afterEach, vi } from "vitest";
+// import userEvent from "@testing-library/user-event";
+import { render, screen } from "@testing-library/react";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import { MemoryRouter, Routes, Route } from "react-router";
+
+// const renderWithRouter = (container) => {
+//   return render(<BrowserRouter>{container}</BrowserRouter>);
+// };
+
+const renderWithRouter = (container) => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: container,
+    },
+  ]);
+
+  return render(
+    <RouterProvider router={router} />
+  );
+};
+
+const mockProps = {
+  cat: { name: "Whiskers" },
+};
+
+afterEach(() => {
+  vi.resetAllMocks();
+});
+
+describe("catTabTemplate", () => {
+  it("Should render component container", () => {
+    renderWithRouter(<CatTabTemplate {...mockProps} />);
+    const catTabCardContainer = screen.getByTestId("cat-card-container");
+    expect(catTabCardContainer).toBeInTheDocument();
+  });
+
+    it("Should render cat name link", () => {
+        renderWithRouter(<CatTabTemplate {...mockProps} />);
+        const catNameLink = screen.getByRole("link", { name: /whiskers/i });
+        expect(catNameLink).toBeInTheDocument();
+    });
+
+    it("Cat name link should have correct href attribute", () => {
+        renderWithRouter(<CatTabTemplate {...mockProps} />);
+        const catNameLink = screen.getByRole("link", { name: /whiskers/i });
+        expect(catNameLink).toHaveAttribute("href", "/cat-view/Whiskers");
+    });
+
+
+
+});
+
