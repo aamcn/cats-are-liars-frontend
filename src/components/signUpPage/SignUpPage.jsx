6004b6b66425bbc143e7e72dfecd9dd31e773685
aaronmcnulty <malcomcrabesq@outlook.com>
2025-09-16 14:26:39 +0100

    Feat: Add SignUpPage component

---
+++ src/components/signUpPage/SignUpPage.jsx
@@ -0,0 +1,13 @@
+import NavBar from "../navbar/NavBar";
+
+
+function SignUpPage() {
+  return (
+    <div className="sign-Up-Page" >
+        <NavBar />
+      <div className="pageFooter"></div>
+    </div>
+  );
+}
+
+export default SignUpPage;
