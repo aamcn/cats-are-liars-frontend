4f695d2551407f58dc808e74dcdd646f9fa2b624
aaronmcnulty <malcomcrabesq@outlook.com>
2025-09-18 16:16:20 +0100

    Refactor: NavBar to use auth0 and display links only when use is logged in and authenticated.

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,39 +1,44 @@
 import { Link } from "react-router";
 import LogOutButton from "./LogoutButton";
 import "./navBar.scss";
-import LoginButton from "../logInForm/logInButton";
+import { useAuth0 } from "@auth0/auth0-react";
+
 // import { useContext } from "react";
 
 // import { appContext } from "../../App";
-function NavBar({logInSuccess, changeLogInSuccess}) {
+function NavBar() {
+  const { isAuthenticated } = useAuth0();
 
   return (
     <div className="navBarContainer" data-testid="navBar">
-      <div className="navLinksContainer">
-       
-        {logInSuccess && (
-          <Link className="navLink" to="/cats-are-liars-frontend/home" data-testid="home-link">
+      {isAuthenticated && (
+        <div className="navLinksContainer">
+          <Link
+            className="navLink"
+            to="/cats-are-liars-frontend/home"
+            data-testid="home-link"
+          >
             Home
           </Link>
-        )}
-        {logInSuccess && (
-          <Link className="navLink" to="/cats-are-liars-frontend/my-cats" data-testid="my-cats-link">
+
+          <Link
+            className="navLink"
+            to="/cats-are-liars-frontend/my-cats"
+            data-testid="my-cats-link"
+          >
             My Cats
           </Link>
-        )}
-        {logInSuccess && (
-          <Link className="navLink" to="/cats-are-liars-frontend/feed-history" data-testid="feed-history-link">
+
+          <Link
+            className="navLink"
+            to="/cats-are-liars-frontend/feed-history"
+            data-testid="feed-history-link"
+          >
             Feed History
           </Link>
-        )}
-         <LogOutButton />
-          <LoginButton />
-          {/* <Link className="navLink" to="/cats-are-liars-frontend/" data-testid="log-in-link" onClick={() => changeLogInSuccess(false)}>
-            Log In
-          </Link> */}
-        
-        {/* {logInSuccess && <LogOutButton />} */}
-      </div>
+          <LogOutButton />
+        </div>
+      )}
     </div>
   );
 }
