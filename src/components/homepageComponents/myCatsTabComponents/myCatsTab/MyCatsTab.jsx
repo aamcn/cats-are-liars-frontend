b036d993710fc225bda7ed3bca6728b7ee080a48
aaronmcnulty <malcomcrabesq@outlook.com>
2025-09-23 14:20:04 +0100

    Feat: MyCatsTab, Add if statement to display a different message if usersCats is empty

--- src/components/homepageComponents/myCatsTabComponents/myCatsTab/MyCatsTab.jsx
+++ src/components/homepageComponents/myCatsTabComponents/myCatsTab/MyCatsTab.jsx
@@ -1,8 +1,8 @@
 import "./myCatsTab.scss";
 import { useState } from "react";
 import CatTabTemplate from "../catTabTemplate/CatTabTemplate.jsx";
-import DownChevron from "../../../../assets/svg/doubleDownChevron.svg?react";
-import MinimiseIcon from "../../../../assets/svg/minimiseIcon.svg?react";
+import DownChevron from "../../../../assets/svg/doubleDownChevron.svg";
+import MinimiseIcon from "../../../../assets/svg/minimiseIcon.svg";
 import { useContext } from "react";
 import { appContext } from "../../../../App.jsx";
 
@@ -18,6 +18,28 @@ function MyCatsTab() {
       setCatsTabVisible(true);
     }
   };
+
+  if(usersCats.length === 0) {
+    return (
+     <div className="homeCatsTabContainer">
+      <div className="homeCatsTab">
+        <h3>My Cats</h3>
+        <button onClick={handleToggleTab} className="homeCatsTabButton">
+          {catsTabVisible ? (
+            <img src={MinimiseIcon} className="minimiseIcon" />
+          ) : (
+            <img src={DownChevron} className="downChevron" />
+          )}
+        </button>
+      </div>
+      {catsTabVisible && (
+        <div className="homeCatCardsContainer">
+            <p>No Cats Found :/</p>
+        </div>
+      )}
+    </div>
+    )
+  }
 
   return (
     <div className="homeCatsTabContainer">
